669bdcc7de27a57fcc71525aee5cf0ee78e64473
Nikolay <nikolay_zakirov@sfu.ca>
2026-08-07 17:32:14 -0700

    Implemented the schema and made changes to the ERD

--- db_api.py
+++ db_api.py
@@ -4,30 +4,64 @@ SCHEMA = """
 CREATE TABLE IF NOT EXISTS Customer(
     customerID INTEGER PRIMARY KEY AUTOINCREMENT,
     firstName VARCHAR(50),
-    lastName VARCHAR(50),
-    email VARCHAR(50),
-    balance FLOAT
+    lastName VARCHAR(50) DEFAULT NULL,
+    email VARCHAR(100) DEFAULT NULL,
+    balance INTEGER DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS Item(
     itemID INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50),
-    type VARCHAR(50),
-    isAvailable BOOLEAN
+    type VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS Records(
+    recordID INTEGER PRIMARY KEY AUTOINCREMENT,
     customerID INTEGER,
     itemID INTEGER,
-    returnDate DATETIME,
-    isReturned BOOLEAN,
+    checkoutDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    returnDate DATETIME DEFAULT NULL,
 
     FOREIGN KEY (customerID) REFERENCES Customer(customerID),
-    FOREIGN KEY (itemID) REFERENCES Item(itemID),
-    PRIMARY KEY (customerID, ItemID)
+    FOREIGN KEY (itemID) REFERENCES Item(itemID)
 );
 
-CREATE TABLE IF NOT EXISTS FutureAddition"""
+CREATE TABLE IF NOT EXISTS FutureAddition(
+    name VARCHAR(50) PRIMARY KEY,
+    type VARCHAR(50),
+    demand INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS Room(
+    location VARCHAR(50) PRIMARY KEY,
+    capacity INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS Event(
+    eventID INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50),
+    type VARCHAR(50),
+    description VARCHAR(300)
+);
+
+CREATE TABLE IF NOT EXISTS Booking(
+    eventID INTEGER,
+    location VARCHAR(50),
+    startTime DATETIME,
+    endTime DATETIME,
+
+    FOREIGN KEY (location) REFERENCES Room(location),
+    FOREIGN KEY (eventID) REFERENCES Event(eventID),
+    PRIMARY KEY (location, startTime)
+);
+
+CREATE TABLE IF NOT EXISTS EventInterest(
+    eventID INTEGER,
+    itemType VARCHAR(50),
+    
+    FOREIGN KEY (eventID) REFERENCES Event(eventID),
+    PRIMARY KEY (eventID, itemType)
+)"""
 
 
 class Database:
